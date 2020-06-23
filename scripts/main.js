f133b297112c60f6b7fb99765d71a5a9112c4fdf
TheKwom <dafwendz@gmail.com>
2020-06-22 19:46:33 -0600

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,15 @@ const toggleMenu = () => {
 }
 
 document.querySelector('#navigation').addEventListener('click', toggleMenu);
+
+function sendAlert() {
+    alert("This is an alert() method!");
+}
+
+function scrollBottom() {
+    window.scrollTo(0,5000);
+}
+
+function scrollBack() {
+    window.scrollTo(0,50);
+}
